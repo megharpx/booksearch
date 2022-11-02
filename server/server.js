3d3df1183f333fa2d1b7bf113e2186168b13aa02
megharpx <meghaneharper@gmail.com>
2022-11-02 18:28:12 -0400

    updated server

--- server/server.js
+++ server/server.js
@@ -1,34 +1,50 @@
-const express = require('express');
+const express = require("express");
+// import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
-const path = require('path');
+const { authMiddleware } = require("./utils/auth");
+
+const path = require("path");
+const db = require("./config/connection");
+
+// const routes = require("./routes");
+
+// import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
-const db = require('./config/connection');
-const { authMiddleware } = require('./utils/auth');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers});
-  server.start().then(res => {
-    server.applyMiddleware({ app });
-    app.listen({ port: 3000 }, () =>
-    console.log("nice"))
-  })
+  resolvers,
+  context: authMiddleware,
+});
+
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/public")));
 }
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/public/index.html"));
 });
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`);
+// app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
+db.on("error", (err) => {
+  console.error("MongoDB connection error: ", err);
 });
